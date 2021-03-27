373844dde1a2f1a37b2c1d1d1e5d3e918a5aeaea
silver_gp@m1 <376980440@qq.com>
2021-03-27 20:20:49 +0800

    regression system

--- InvestmentAnalystSystem/FactorLinearAnalyst.py
+++ InvestmentAnalystSystem/FactorLinearAnalyst.py
@@ -1,5 +1,15 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import torch
+import torch.nn as nn
+
+class FactorLinearNN(nn.Module):
+    def __init__(self, feature_num :int):
+        super(FactorLinearNN, self).__init__()
+        self.mFeatureNum = feature_num
+        self.fc1 = nn.Linear(feature_num, 1)
+
+    
 
 class FactorLinearAnalyst:
     def __init__(self):
---
+++ InvestmentAnalystSystem/StockRegressionSystem.py
@@ -0,0 +1,11 @@
+class StockRegressionSystem:
+    def __init__(self):
+        pass
+
+    def Fit(self, X, y):
+        pass
+
+    def Predict(self, X):
+        pass
+
+    
