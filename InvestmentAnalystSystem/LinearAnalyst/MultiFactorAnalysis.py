1198f9936649063043207ca3508b7b1f4d71c86d
silver_gp <silver_gp@live.com>
2021-04-13 14:14:54 +0800

    multifactor analysis

--- InvestmentAnalystSystem/LinearAnalyst/MultiFactorAnalysis.py
+++ InvestmentAnalystSystem/LinearAnalyst/MultiFactorAnalysis.py
@@ -1,19 +1,27 @@
 from InvestmentAnalystSystem.Common.NNRegressionSystem import NNRegressionSystem
 from InvestmentAnalystSystem.LinearAnalyst.FactorLinearNN import FactorLinearNN
+from Data.UseData import read_data
 import torch
 import torch.nn as nn
+import torch.optim as optim
 
-class MultiFactorAnalysis:
-    def __init__(self):
-        pass
-
-    def Fit(self):
-        pass
 
 
 def Main():
     solver = NNRegressionSystem()
-    network = FactorLinearNN(3)
+    model = FactorLinearNN(1)
+    optimizer = optim.SGD(model.parameters(), lr=1)
     loss_func = nn.MSELoss()
+    solver.Init(model, optimizer, loss_func )
+    
 
+    reader = read_data()
+    result=reader.get_daily_data('sh',start_date=20210405,end_date=20210412,distance=1)
+    print(result)
+
+    #solver.Fit()
+    
+    
     pass
+
+Main()
