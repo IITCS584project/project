00d88954aaf1701194124c35e885949ad90a1fd7
silver_gp@m1 <376980440@qq.com>
2021-04-19 20:17:11 +0800

    stock data provider

--- InvestmentAnalystSystem/Common/StockDataProvider.py
+++ InvestmentAnalystSystem/Common/StockDataProvider.py
@@ -5,11 +5,16 @@ class StockDataProvider:
     @staticmethod
     def GetStockDataForPredict( asset_ticker, market_ticker, start_date, end_date ):
         reader = read_data()
-        today_data = reader.get_daily_data( [asset_ticker] ,start_date, end_date, 1,['ts_code', 'trade_date', 'rate_of_increase'])
-        yesterday_data = reader.get_daily_data( [asset_ticker] ,start_date, end_date, 1,['vol'])
-        # remove 1st row 
-        today_data = today_data[1:]
-        yesterday_data = yesterday_data[:-1,:]
-        stock_data = np.concatenate([today_data, yesterday_data], axis=1)
-        return stock_data
+        asset_data = reader.get_daily_data( [asset_ticker, market_ticker] ,start_date, end_date, 1,
+                    ['ts_code', 'trade_date', 'rate_of_increase', 'vol'])        
+        # remove 1st row, the 1st yield is na
+        yield_data = asset_data[0, 1:, 2]        
+        # remove the last row,I use the chracteristics of yesterday to predict today's price
+        yesterday_data = asset_data[0, :-1,3:]
+        # use the market data of yesterday to predict today's price
+        market_data = asset_data[1, :-1, 2]
+
+        X = yesterday_data
+        y = yield_data
+        return X, y
     
